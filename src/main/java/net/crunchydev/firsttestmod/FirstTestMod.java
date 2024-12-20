46fe7b837f1993cd73dde209215b60d328787a2d
Crunchy <jpwill2004@gmail.com>
2024-12-19 22:03:14 -0600

    Initial commit

---
+++ src/main/java/net/crunchydev/firsttestmod/FirstTestMod.java
@@ -0,0 +1,18 @@
+package net.crunchydev.firsttestmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FirstTestMod implements ModInitializer {
+
+	public static final String MOD_ID = "firsttestmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+
+}
---
+++ src/main/java/net/crunchydev/firsttestmod/FirstTestModClient.java
@@ -0,0 +1,12 @@
+package net.crunchydev.firsttestmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FirstTestModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+
+}
